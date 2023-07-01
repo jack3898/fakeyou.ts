e63ecf833f4225668755b5a515c0fb3963d11e80
Jack Wright <28375223+jack3898@users.noreply.github.com>
2023-07-01 16:57:25 +0100

    change syntax for logging

--- src/util/log.ts
+++ src/util/log.ts
@@ -7,22 +7,22 @@ let enabled = false;
 export const log = {
 	error(message: string) {
 		if (enabled) {
-			console.error(`${chalk.bgRed(' ERROR ')} ${message}`);
+			console.error(`${chalk.bgRed` ERROR `} ${message}`);
 		}
 	},
 	success(message: string) {
 		if (enabled) {
-			console.log(`${chalk.bgGreen(' SUCCESS ')} ${message}`);
+			console.log(`${chalk.bgGreen` SUCCESS `} ${message}`);
 		}
 	},
 	info(message: string) {
 		if (enabled) {
-			console.log(`${chalk.bgBlue(' INFO ')} ${message}`);
+			console.log(`${chalk.bgBlue` INFO `} ${message}`);
 		}
 	},
 	warn(message: string) {
 		if (enabled) {
-			console.log(`${chalk.bgYellow(' WARN ')} ${message}`);
+			console.log(`${chalk.bgYellow` WARN `} ${message}`);
 		}
 	},
 	http(url: URL) {
