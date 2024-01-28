8ac571a0c44d1e874ff4cfc00be2333d2a815866
Jack Wright <28375223+jack3898@users.noreply.github.com>
2024-01-28 13:32:42 +0000

    chore(#229): remove image generator for spectrogram

--- src/api/ttsResult/Spectrogram/Spectrogram.ts
+++ src/api/ttsResult/Spectrogram/Spectrogram.ts
@@ -1,9 +1,4 @@
-import { createCanvas, type Canvas, type CanvasRenderingContext2D } from 'canvas';
-import fs from 'node:fs';
-import { promisify } from 'node:util';
 import { type MelMatrix } from './spectrogram.schema.js';
-
-const writeFile = promisify(fs.writeFile);
 
 /**
  * A RGBA color, represented as an array of 4 8-bit numbers (0-255) in the order [red, green, blue, alpha].
@@ -22,16 +17,12 @@ export class Spectrogram {
 	 */
 	constructor(data: MelMatrix) {
 		this.data = data;
-		this.#canvas = createCanvas(this.width, this.height);
-		this.#canvasContext = this.#canvas.getContext('2d');
 	}
 
 	/**
 	 * The spectrogram data in its raw form. A 2D array of 8-bit numbers.
 	 */
 	readonly data: MelMatrix;
-	readonly #canvas: Canvas;
-	readonly #canvasContext: CanvasRenderingContext2D;
 
 	/**
 	 * Get the width of the spectrogram output.
@@ -49,39 +40,5 @@ export class Spectrogram {
 
 	get rgba(): RGBA[][] {
 		return this.data.map((row) => row.map((col): RGBA => [col, col, col, 255]));
-	}
-
-	/**
-	 * Get the spectrogram as a canvas.
-	 */
-	get canvas(): Canvas {
-		const imageData = this.#canvasContext.createImageData(this.width, this.height);
-
-		imageData.data.set(new Uint8ClampedArray(this.rgba.flat(2)));
-
-		this.#canvasContext.putImageData(imageData, 0, 0);
-
-		return this.#canvas;
-	}
-
-	/**
-	 * Get the spectrogram as a PNG buffer.
-	 *
-	 * The image is not altered or processed in any way, it is simply an interpretation of the raw data.
-	 */
-	get png(): Buffer {
-		return this.canvas.toBuffer('image/png');
-	}
-
-	/**
-	 * Save the spectrogram PNG to disk.
-	 *
-	 * The image is not altered or processed in any way, it is simply an interpretation of the raw data.
-	 *
-	 * @param location The location to save the spectrogram to.
-	 * @returns A promise that resolves when the spectrogram has been saved.
-	 */
-	toDisk(location: `${string}.png`): Promise<void> {
-		return writeFile(location, this.png);
 	}
 }
